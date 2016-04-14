257e307a56ddcdb75f7c9d085c7bce058d5b318a
philippspo <philipp.sporrer@commercetools.de>
2016-04-14 12:21:57 +0200

    feat(execute): add basic service lifecycle workflow, add corresponding tests

--- src/index.js
+++ src/index.js
@@ -1,5 +1,48 @@
-export default {
-  execute({ payload, run, onComplete }) {
-    return { payload, run, onComplete }
+import initAjv from 'ajv'
+
+const ajv = initAjv({ removeAdditional: true })
+export const validate = ajv.compile({
+  type: 'object',
+  properties: {
+    assets: {
+      type: 'object',
+      patternProperties: {
+        '.*': {
+          type: 'string',
+        },
+      },
+    },
+    options: {
+      type: 'object',
+    },
   },
+  required: ['options'],
+  additionalProperties: false,
+})
+
+export const moveAssetsToOptions = ({ options, assets }) =>
+  Object.assign({}, options, assets)
+
+export const defaultConfig = {}
+
+export default function (/* customConfig = {} */) {
+  // const config = Object.assign({}, defaultConfig, customConfig)
+  return {
+    execute({ payload, onRun, onComplete, onError }) {
+      // validate payload
+      const valid = validate(payload)
+      if (!valid) {
+        onError(validate.errors)
+        return Promise.resolve()
+      }
+      // move all assets to options
+      const optionsWithAssets = moveAssetsToOptions(payload)
+      // call onRun method with options
+      return onRun(optionsWithAssets).then(() => {
+        if (onComplete) {
+          onComplete()
+        }
+      })
+    },
+  }
 }
--- tests/iron-container.spec.js
+++ tests/iron-container.spec.js
@@ -1,21 +1,66 @@
 import test from 'ava'
-import container from '../src'
+import container, { validate, moveAssetsToOptions } from '../src'
+import sinon from 'sinon'
 
-test('should export an object with a `execute` function', t => {
-  t.is(typeof container, 'object', 'container should be an object')
-  t.is(typeof container.execute, 'function', 'container.execute should be a function')
+test('container - should export an object with a `execute` function', t => {
+  t.is(typeof container, 'function', 'container should be a function')
+  t.is(typeof container(), 'object', 'container() should return an object')
+  t.is(typeof container().execute, 'function', 'container().execute should be a function')
 })
 
-test('export function should return the given param', t => {
-  const config = { payload: 'a', run: () => null, onComplete: () => null }
-  const actual = container.execute(config)
-
-  t.deepEqual(actual, config)
+test('container - should call the onRun callback after the payload is processed', t => {
+  const payload = { options: {} }
+  const onRun = sinon.stub().returns(Promise.resolve())
+  container().execute({ payload, onRun })
+  t.is(onRun.calledOnce, true)
+  t.deepEqual(onRun.firstCall.args[0], {})
 })
 
-// test(t => {
-//     const a = /foo/;
-//     const b = 'bar';
-//     const c = 'baz';
-//     t.true(a.test(b) || b === c);
-// });
+test('container - should call the onError callback if the payload is invalid', t => {
+  const payload = {}
+  const onError = sinon.spy()
+  container().execute({ payload, onError })
+  t.is(onError.calledOnce, true)
+  t.is(Array.isArray(onError.firstCall.args[0]), true)
+})
+
+test('container - should call the onComplete callback if the service finished', t => {
+  const payload = { options: {} }
+  const onRun = () => Promise.resolve()
+  const onComplete = sinon.spy()
+  container().execute({ payload, onRun, onComplete })
+  .then(() => {
+    t.is(onComplete.calledOnce, true)
+  })
+})
+
+test('validate - should accept a payload with an empty options array', t => {
+  const actual = validate({ options: {} })
+
+  t.is(actual, true)
+})
+
+test('validate - should accept a payload with multiple assets', t => {
+  const payload = {
+    options: {},
+    assets: { inputFileOne: 'http://url.com', inputFileTwo: 'http://url2.com' },
+  }
+  const actual = validate(payload)
+
+  t.is(actual, true)
+})
+
+test('moveAssetsToOptions - should add assets to options', t => {
+  const payload = {
+    options: { someOption: true },
+    assets: { inputFileOne: 'http://url.com', inputFileTwo: 'http://url2.com' },
+  }
+  const options = {
+    someOption: true,
+    inputFileOne: 'http://url.com',
+    inputFileTwo: 'http://url2.com',
+  }
+  const actual = moveAssetsToOptions(payload)
+
+  t.deepEqual(actual, options)
+})
