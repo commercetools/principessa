7b5dd4dc2136b63bad59a1953305258e0b5de128
philippspo <philipp.sporrer@commercetools.de>
2016-05-04 13:32:20 +0200

    fix(aws): use os temp folder as default downloadFolder, fix signature version to v4

--- src/storage-providers/aws.js
+++ src/storage-providers/aws.js
@@ -2,14 +2,22 @@ import AWS from 'aws-sdk'
 import path from 'path'
 import cuid from 'cuid'
 import { createWriteStream, createReadStream } from 'fs'
+import temp from 'temp'
+
+// Automatically track and cleanup files at exit
+temp.track()
 
 /*
  * this requires AWS credentials being set in the environment
  * AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
  */
 export default (config = {}) => {
-  const { bucket: Bucket, downloadFolder, awsConfig } = config
-  const s3 = new AWS.S3(awsConfig)
+  const { bucket: Bucket, awsConfig } = config
+  let { downloadFolder } = config
+  if (!downloadFolder) {
+    downloadFolder = temp.mkdirSync()
+  }
+  const s3 = new AWS.S3(Object.assign({}, awsConfig, { signatureVersion: 'v4' }))
   return {
     download(file) {
       return new Promise((resolve, reject) => {
